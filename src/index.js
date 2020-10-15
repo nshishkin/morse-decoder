1a92df90f310b6e86484d451725287b674a64aac
Nikolay Shishkin <spwawnick@gmail.com>
2020-10-15 16:02:16 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultStr = '';
+    let arr = expr.match(/.{1,10}/g);
+    for (let i = 0; i < arr.length; i += 1){
+        if (arr[i] === "**********"){
+            resultStr += ' ';
+        } else{
+            arr[i] = Number(arr[i]).toString();
+            arr[i] = arr[i].replace(/(10)/g, '.');
+            arr[i] = arr[i].replace(/(11)/g, '-');
+            resultStr += MORSE_TABLE[arr[i]]; 
+        }
+    }
+    return resultStr;
 }
 
 module.exports = {
